9dc96e42ee287de791231c298d1fe3dfde951f07
imorenodev <imorenodev@gmail.com>
2016-02-16 14:27:45 -0500

    added comments

---
+++ monads-practice.js
@@ -0,0 +1,15 @@
+//Introduction to Monads with Douglas Crockford's
+//course - Javascript The Good Parts
+
+function MONAD() {
+  return function unit(value) {
+    var monad = Object.create(null);
+    monad.bind = function (func) {
+      return func(value);
+    };
+    return monad;
+  };
+}
+var unit = MONAD();
+var monad = unit("Hello World!");
+monad.bind(alert);
