09c92e5e3c81a367366d1907be3aa2e1f2a39a98
imorenodev@gmail.com <imorenodev@gmail.com>
2016-04-25 14:33:47 -0400

    practice with reducers

--- reducers.js
+++ reducers.js
@@ -1,2 +1,39 @@
 const sum = (acc, x) => acc + x;
 console.log([1,2,4].reduce(sum,0));
+
+
+var foods = [
+  'apples',
+  'oranges',
+  'pizza',
+  'cereal',
+  'apples',
+  'oranges',
+  'pizza',
+  'cereal',
+  'apples',
+  'oranges',
+  'apples',
+  'oranges',
+  'pizza',
+  'cereal',
+  'apples',
+  'oranges',
+  'pizza',
+  'cereal',
+  'apples',
+  'oranges'
+];
+
+var reducer = function(tally, vote) {
+  console.log('Tally ', tally, ' Vote ', vote);
+  if (!tally[vote]) {
+    tally[vote] = 1;
+  } else {
+    tally[vote] = tally[vote] + 1;
+  }
+  return tally;
+}
+
+var result = foods.reduce(reducer, {})
+console.log(result);
